7c41f242a1fb3f4d941514cc61c73830b109a9b6
TacB0sS <adam.vdk@nu-art-software.com>
2021-01-10 21:20:09 +0200

    only ts-config

--- .scripts/modules.sh
+++ .scripts/modules.sh
@@ -1,11 +1,12 @@
 projectModules=(
-  app-backend
-  app-frontend
+  #  app-backend
+  #  app-frontend
 )
 
 projectLibraries=(
-  ${thunderstormLibraries[@]}
-  app-shared
+  #  ${thunderstormLibraries[@]}
+  #  app-shared
+  ts-common
 )
 
 testServiceAccount=../.trash/test-account.json
