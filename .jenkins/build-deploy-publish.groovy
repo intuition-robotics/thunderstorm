ec8a2306fec0228980b3fcd6972c1b0defd4e0af
AndreiHardziyenkaIR <your_email_address@example.com>
2022-07-10 19:29:13 +0300

    testing it.description

--- .jenkins/build-deploy-publish.groovy
+++ .jenkins/build-deploy-publish.groovy
@@ -36,9 +36,9 @@ class Pipeline_Build
 	void _postInit() {
 		TriggerCause[] causes = getModule(BuildModule.class).getTriggerCause(TriggerCause.Type_SCM)
 		this.logInfo("GOT HERE!! ${causes.size()}")
-		TriggerCause cause = causes.find { it.print() == "Cause(org.jenkinsci.plugins.gwt.GenericCause): AndreiHardziyenkaIR Pushed to prod" }
+		TriggerCause cause = causes.find { it.description == "AndreiHardziyenkaIR Pushed to prod" }
 		causes.each {
-			this.logInfo("Detected SCM cause: '${it.type}'")
+			this.logInfo("Detected SCM cause: '${it.description}'")
 		}
 
 		if (cause) {
