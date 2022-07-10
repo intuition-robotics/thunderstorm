eb41d35c675bfa04287c753e3cd39b3a5a3bcb2b
AndreiHardziyenkaIR <your_email_address@example.com>
2022-07-10 19:22:14 +0300

    testing unknown cause

--- .jenkins/build-deploy-publish.groovy
+++ .jenkins/build-deploy-publish.groovy
@@ -36,9 +36,9 @@ class Pipeline_Build
 	void _postInit() {
 		TriggerCause[] causes = getModule(BuildModule.class).getTriggerCause(TriggerCause.Type_SCM)
 		this.logInfo("GOT HERE!! ${causes.size()}")
-		TriggerCause cause = causes.find { it == "Cause(org.jenkinsci.plugins.gwt.GenericCause): IR-Jenkins Pushed to prod" }
+		TriggerCause cause = causes.find { it.data == "AndreiHardziyenkaIR Pushed to prod" }
 		causes.each {
-			this.logInfo("Detected SCM cause: '${it.originator}'")
+			this.logInfo("Detected SCM cause: '${it.data}'")
 		}
 
 		if (cause) {
