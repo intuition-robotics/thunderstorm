4e36bbae135b1286d4de17a93c51249cd920b27d
AndreiHardziyenkaIR <your_email_address@example.com>
2022-07-10 17:18:56 +0300

    changing originator

--- .jenkins/build-deploy-publish.groovy
+++ .jenkins/build-deploy-publish.groovy
@@ -36,7 +36,7 @@ class Pipeline_Build
 	void _postInit() {
 		TriggerCause[] causes = getModule(BuildModule.class).getTriggerCause(TriggerCause.Type_SCM)
 		this.logInfo("GOT HERE!! ${causes.size()}")
-		TriggerCause cause = causes.find { it.originator == "Nu-Art-Jenkins" }
+		TriggerCause cause = causes.find { it.originator == "IR-Jenkins" }
 		causes.each {
 			this.logInfo("Detected SCM cause: '${it.originator}'")
 		}
