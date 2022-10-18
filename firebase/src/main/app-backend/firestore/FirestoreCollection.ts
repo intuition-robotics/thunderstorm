7aa5a9b1e04a0c34a74d886ed118d3eefb4f2c1e
Alan Ben <abseralvo@gmail.com>
2022-10-18 15:02:57 +0300

    delete returns doc deleted

--- firebase/src/main/app-backend/firestore/FirestoreCollection.ts
+++ firebase/src/main/app-backend/firestore/FirestoreCollection.ts
@@ -117,21 +117,20 @@ export class FirestoreCollection<Type extends object> {
 		return this.runInTransaction((transaction) => transaction.deleteUnique(this, query))
 	}
 
-	async delete(query: FirestoreQuery<Type>): Promise<number> {
+	async delete(query: FirestoreQuery<Type>): Promise<Type[]> {
 		const docRefs = await this._query(query);
 		return this.deleteBatch(docRefs);
 	}
 
-	private async deleteBatch(docRefs: FirestoreType_DocumentSnapshot[]): Promise<number> {
+	private async deleteBatch(docRefs: FirestoreType_DocumentSnapshot[]): Promise<Type[]> {
 		await batchAction(docRefs, 200, async (temp) => {
 			const initialValue = this.wrapper.firestore.batch();
-			// @ts-ignore
 			await temp.reduce((batch, val) => batch.delete(val.ref), initialValue).commit();
 		})
-		return docRefs.length;
+		return docRefs.map(d => d.data() as Type);
 	}
 
-	async deleteAll(): Promise<number> {
+	async deleteAll(): Promise<Type[]> {
 		const docRefs = await this._query();
 		return this.deleteBatch(docRefs);
 	}
