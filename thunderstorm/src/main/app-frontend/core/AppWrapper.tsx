07c094c051cbbd790de112e81da46fa84b8980f5
Alan Ben <abseralvo@gmail.com>
2023-12-13 11:08:05 +0200

    render app

--- thunderstorm/src/main/app-frontend/core/AppWrapper.tsx
+++ thunderstorm/src/main/app-frontend/core/AppWrapper.tsx
@@ -1,49 +1,29 @@
-/*
- * Thunderstorm is a full web app framework!
- *
- * Typescript & Express backend infrastructure that natively runs on firebase function
- * Typescript & React frontend infrastructure
- *
- * Copyright (C) 2020 Intuition Robotics
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
 import {Router} from 'react-router-dom';
 // noinspection TypeScriptPreferShortImport`
 import {BrowserHistoryModule} from "../modules/HistoryModule";
 import {Thunder} from "./Thunder";
 import {ImplementationMissingException} from "@intuitionrobotics/ts-common";
-
+import {createRoot} from "react-dom/client";
 
 export type WrapperProps = {}
 export const AppWrapper = (props: WrapperProps) => {
 
-	const MainApp = Thunder.getInstance().getMainApp();
-	if (!MainApp)
-		throw new ImplementationMissingException("mainApp was not specified!!");
+    const MainApp = Thunder.getInstance().getMainApp();
+    if (!MainApp)
+        throw new ImplementationMissingException("mainApp was not specified!!");
 
-	return (
-		<Router history={BrowserHistoryModule.getHistory()}>
-			<MainApp/>
-		</Router>)
+    return (
+        <Router history={BrowserHistoryModule.getHistory()}>
+            <MainApp/>
+        </Router>)
 };
 
 export function renderApp(appId: string = "app") {
-	ReactDOM.render(
-		<AppWrapper/>,
-		document.getElementById(appId)
-	);
+    const el = document.getElementById(appId);
+    if (!el)
+        throw new Error(`Could not find element with id ${appId}`);
+
+    const root = createRoot(el);
+    root.render(<AppWrapper/>)
 }
